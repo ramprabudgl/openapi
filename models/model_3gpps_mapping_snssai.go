5f25f00864f990a8de880bb89acdc432c5ad896d
ramprabudgl <67000416+ramprabudgl@users.noreply.github.com>
2024-08-30 12:32:12 +0530

    Update model_3gpps_mapping_snssai.go
    solve the duplicate name of type MappingOfSnssai

--- models/model_3gpps_mapping_snssai.go
+++ models/model_3gpps_mapping_snssai.go
@@ -1,4 +1,4 @@
-type MappingOfSnssai struct {
+type ExtendMappingOfSnssai struct {
 	ServingSnssai_extention *ExtendSnssai `json:"servingSnssai" bson:"servingSnssai" yaml:"servingSnssai"`
 
 	HomeSnssai_extention *ExtendSnssai `json:"homeSnssai" bson:"homeSnssai" yaml:"homeSnssai"`
