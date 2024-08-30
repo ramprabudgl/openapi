e2f16cfd32c5826b3fdd755b52e192cd84125b22
ramprabudgl <67000416+ramprabudgl@users.noreply.github.com>
2024-08-30 11:35:33 +0530

    Create model_3gpps_mapping_snssai.go
    Added this file to support for 16 version of 3gpp for integration of Nid

---
+++ models/model_3gpps_mapping_snssai.go
@@ -0,0 +1,5 @@
+type MappingOfSnssai struct {
+	ServingSnssai_extention *ExtendSnssai `json:"servingSnssai" bson:"servingSnssai" yaml:"servingSnssai"`
+
+	HomeSnssai_extention *ExtendSnssai `json:"homeSnssai" bson:"homeSnssai" yaml:"homeSnssai"`
+}
