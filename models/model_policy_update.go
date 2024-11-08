37c278e98664cfc79f78dbac9f2f1dcc003ba20f
ramprabudgl <67000416+ramprabudgl@users.noreply.github.com>
2024-11-08 14:59:07 +0530

    Update model_policy_update.go
    New parameters have been added according to Release 16 requirements.

--- models/model_policy_update.go
+++ models/model_policy_update.go
@@ -17,4 +17,16 @@ type PolicyUpdate struct {
 	Rfsp        int32                   `json:"rfsp,omitempty" yaml:"rfsp" bson:"rfsp" mapstructure:"Rfsp"`
 	// Map of PRA information.
 	Pras map[string]PresenceInfoRm `json:"pras,omitempty" yaml:"pras" bson:"pras" mapstructure:"Pras"`
+        // extra parameters -Hemanth
+	AltNotifFqdns     []string          `json:"altNotifFqdns,omitempty" yaml:"altNotifFqdns" bson:"altNotifFqdns" mapstructure:"AltNotifFqdns"`
+	SmfSelInfo        *SmfSelectionData `json:"smfSelInfo,omitempty" yaml:"smfSelInfo" bson:"smfSelInfo" mapstructure:"SmfSelInfo"`
+	UeAmbr            *Ambr             `json:"ueAmbr,omitempty" yaml:"ueAmbr" bson:"ueAmbr" mapstructure:"UeAmbr"`
+	AllowedSnssais    []Snssai          `json:"allowedSnssais,omitempty" yaml:"allowedSnssais" bson:"allowedSnssais" mapstructure:"AllowedSnssais"`
+	MappingSnssais    []MappingOfSnssai `json:"mappingSnssais,omitempty" yaml:"mappingSnssais" bson:"mappingSnssais" mapstructure:"MappingSnssais"`
+	N3gAllowedSnssais []Snssai          `json:"n3gAllowedSnssais,omitempty" yaml:"n3gAllowedSnssais" bson:"n3gAllowedSnssais" mapstructure:"N3gAllowedSnssais"`
+	AccessTypes       []AccessType      `json:"accessTypes,omitempty" yaml:"accessTypes" bson:"accessTypes" mapstructure:"AccessTypes"`
+	Guami             *Guami            `json:"guami,omitempty" yaml:"guami" bson:"guami" mapstructure:"Guami"`
+	UserLoc           *UserLocation     `json:"userLoc,omitempty" yaml:"userLoc" bson:"userLoc" mapstructure:"UserLoc"`
+	TraceReq          *TraceData        `json:"traceReq,omitempty" yaml:"traceReq" bson:"traceReq" mapstructure:"TraceReq"`
+
 }
