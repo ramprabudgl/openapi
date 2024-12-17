0c1ad3d881700ac9d1a2e3df5c9946acac0562f5
ramprabudgl <67000416+ramprabudgl@users.noreply.github.com>
2024-12-17 12:18:34 +0530

    Update api_default.go
    added new method for terminatedelete from amf to pcf

--- Npcf_AMPolicy/api_default.go
+++ Npcf_AMPolicy/api_default.go
@@ -156,6 +156,132 @@ func (a *DefaultApiService) PoliciesPolAssoIdDelete(ctx context.Context, polAsso
 	}
 }
 
+
+func (a *DefaultApiService) PoliciesPolAssoIdDeleteTerminate(ctx context.Context, polAssoId string, headers map[string]string) (*http.Response, error) {
+	var (
+		localVarHTTPMethod   = strings.ToUpper("Delete")
+		localVarPostBody     interface{}
+		localVarFormFileName string
+		localVarFileName     string
+		localVarFileBytes    []byte
+	)
+
+	// create path and map variables
+	localVarPath := a.client.cfg.BasePath() + "/policies/{polAssoId}"
+	localVarPath = strings.Replace(localVarPath, "{"+"polAssoId"+"}", fmt.Sprintf("%v", polAssoId), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	for key, value := range headers {
+		localVarHeaderParams[key] = value
+	}
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+
+	localVarHTTPContentTypes := []string{"application/json"}
+
+	localVarHeaderParams["Content-Type"] = localVarHTTPContentTypes[0] // use the first content type specified in 'consumes'
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/problem+json"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := openapi.SelectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+
+	r, err := openapi.PrepareRequest(ctx, a.client.cfg, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFormFileName, localVarFileName, localVarFileBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	localVarHTTPResponse, err := openapi.CallAPI(a.client.cfg, r)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarHTTPResponse, err
+	}
+
+	localVarBody, err := ioutil.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	if err != nil {
+		return localVarHTTPResponse, err
+	}
+
+	apiError := openapi.GenericOpenAPIError{
+		RawBody:     localVarBody,
+		ErrorStatus: localVarHTTPResponse.Status,
+	}
+
+	switch localVarHTTPResponse.StatusCode {
+	case 204:
+		return localVarHTTPResponse, nil
+	case 400:
+		var v models.ProblemDetails
+		err = openapi.Deserialize(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+		if err != nil {
+			apiError.ErrorStatus = err.Error()
+			return localVarHTTPResponse, apiError
+		}
+		apiError.ErrorModel = v
+		return localVarHTTPResponse, apiError
+	case 401:
+		var v models.ProblemDetails
+		err = openapi.Deserialize(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+		if err != nil {
+			apiError.ErrorStatus = err.Error()
+			return localVarHTTPResponse, apiError
+		}
+		apiError.ErrorModel = v
+		return localVarHTTPResponse, apiError
+	case 403:
+		var v models.ProblemDetails
+		err = openapi.Deserialize(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+		if err != nil {
+			apiError.ErrorStatus = err.Error()
+			return localVarHTTPResponse, apiError
+		}
+		apiError.ErrorModel = v
+		return localVarHTTPResponse, apiError
+	case 404:
+		var v models.ProblemDetails
+		err = openapi.Deserialize(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+		if err != nil {
+			apiError.ErrorStatus = err.Error()
+			return localVarHTTPResponse, apiError
+		}
+		apiError.ErrorModel = v
+		return localVarHTTPResponse, apiError
+	case 429:
+		var v models.ProblemDetails
+		err = openapi.Deserialize(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+		if err != nil {
+			apiError.ErrorStatus = err.Error()
+			return localVarHTTPResponse, apiError
+		}
+		apiError.ErrorModel = v
+		return localVarHTTPResponse, apiError
+	case 500:
+		var v models.ProblemDetails
+		err = openapi.Deserialize(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+		if err != nil {
+			apiError.ErrorStatus = err.Error()
+			return localVarHTTPResponse, apiError
+		}
+		apiError.ErrorModel = v
+		return localVarHTTPResponse, apiError
+	case 503:
+		var v models.ProblemDetails
+		err = openapi.Deserialize(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+		if err != nil {
+			apiError.ErrorStatus = err.Error()
+			return localVarHTTPResponse, apiError
+		}
+		apiError.ErrorModel = v
+		return localVarHTTPResponse, apiError
+	default:
+		return localVarHTTPResponse, nil
+	}
+}
+
 /*
 DefaultApiService
  * @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
